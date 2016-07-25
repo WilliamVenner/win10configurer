9414ed8427e9a570b289e0a18d493b1fb5ce9277
William Venner <william@venner.io>
2016-07-25 01:33:04 +0100

    Initial commit

---
+++ Windows 10 Configurer/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Windows_10_Configurer
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.OptionsList = new System.Windows.Forms.CheckedListBox();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.ReinstallButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Windows 10 Configurer";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(334, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "By William Venner (@ven_ner)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OptionsList
+            // 
+            this.OptionsList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.OptionsList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.OptionsList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OptionsList.ForeColor = System.Drawing.Color.White;
+            this.OptionsList.FormattingEnabled = true;
+            this.OptionsList.Items.AddRange(new object[] {
+            "Install Windows Photo Viewer"});
+            this.OptionsList.Location = new System.Drawing.Point(12, 100);
+            this.OptionsList.Name = "OptionsList";
+            this.OptionsList.Size = new System.Drawing.Size(310, 255);
+            this.OptionsList.TabIndex = 2;
+            this.OptionsList.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.OptionsList_ItemCheck);
+            this.OptionsList.Click += new System.EventHandler(this.OptionsList_Click);
+            // 
+            // GoButton
+            // 
+            this.GoButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.GoButton.BackColor = System.Drawing.Color.Green;
+            this.GoButton.Enabled = false;
+            this.GoButton.FlatAppearance.BorderSize = 0;
+            this.GoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GoButton.ForeColor = System.Drawing.Color.White;
+            this.GoButton.Location = new System.Drawing.Point(12, 419);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(310, 30);
+            this.GoButton.TabIndex = 3;
+            this.GoButton.Text = "Go";
+            this.GoButton.UseVisualStyleBackColor = false;
+            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+            // 
+            // ReinstallButton
+            // 
+            this.ReinstallButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ReinstallButton.BackColor = System.Drawing.Color.Green;
+            this.ReinstallButton.FlatAppearance.BorderSize = 0;
+            this.ReinstallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ReinstallButton.ForeColor = System.Drawing.Color.White;
+            this.ReinstallButton.Location = new System.Drawing.Point(12, 372);
+            this.ReinstallButton.Name = "ReinstallButton";
+            this.ReinstallButton.Size = new System.Drawing.Size(310, 30);
+            this.ReinstallButton.TabIndex = 4;
+            this.ReinstallButton.Text = "Reinstall all apps";
+            this.ReinstallButton.UseVisualStyleBackColor = false;
+            this.ReinstallButton.Click += new System.EventHandler(this.ReinstallButton_Click);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.Controls.Add(this.ReinstallButton);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.OptionsList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1000, 500);
+            this.MinimumSize = new System.Drawing.Size(350, 500);
+            this.Name = "Window";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows 10 Configurer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox OptionsList;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.Button ReinstallButton;
+    }
+}
+
---
+++ Windows 10 Configurer/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Management.Automation;
+using Microsoft.Win32;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System;
+using System.Security.Principal;
+
+namespace Windows_10_Configurer
+{
+    public partial class Window : Form
+    {
+        int LastCheckedIndex;
+        Dictionary<string, string> Config = new Dictionary<string, string>();
+        StringBuilder DisabledApps = new StringBuilder();
+        static bool IsElevated => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+        StringBuilder PowerShellScript = new StringBuilder();
+
+        public Window()
+        {
+
+            if (!IsElevated)
+            {
+                MessageBox.Show("You need to be running Windows 10 Configurer with administrator privileges in order to use the program.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+                return;
+            }
+
+            InitializeComponent();
+
+            string ConfigJSON = Properties.Resources.config;
+            string[] splitn = ConfigJSON.Split('\n');
+            foreach (string line in splitn)
+            {
+                string[] split = line.Split('>');
+                bool m = false;
+                int i = 0;
+                foreach (string v in split)
+                {
+                    m = !m;
+                    if (m == true)
+                    {
+                        Config.Add(v, split[i + 1]);
+                    }
+                    i += 1;
+                }
+            }
+
+            foreach(KeyValuePair<string, string> conf in Config)
+                OptionsList.Items.Add("Disable " + conf.Value);
+
+        }
+
+        private void ReinstallButton_Click(object sender, EventArgs e)
+        {
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+                OptionsList.Enabled = false;
+                GoButton.Enabled = false;
+                ReinstallButton.Enabled = false;
+                using (FileStream PSFile = File.Create(Path.GetTempPath() + "windows10configurerpowershell_reinstall.ps1"))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes("Get-AppxPackage -AllUsers| Foreach {Add-AppxPackage -DisableDevelopmentMode -Register \"$($_.InstallLocation)\\AppXManifest.xml\"}");
+                    PSFile.Write(info, 0, info.Length);
+                }
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "powershell.exe";
+                psi.Arguments = "& '" + Path.GetTempPath() + "windows10configurerpowershell_reinstall.ps1'";
+                Process process = new Process();
+                process.StartInfo.Verb = "runas";
+                process.EnableRaisingEvents = true;
+                process.Exited += new EventHandler(PowerShellExited_Reinstall);
+                process.StartInfo = psi;
+                process.Start();
+            }
+        }
+
+        private void PowerShellExited_Reinstall(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully reinstalled all apps.");
+            OptionsList.Invoke((Action)delegate
+            {
+                OptionsList.Enabled = true;
+            });
+            GoButton.Invoke((Action)delegate
+            {
+                GoButton.Enabled = true;
+            });
+            ReinstallButton.Invoke((Action)delegate
+            {
+                ReinstallButton.Enabled = true;
+            });
+            File.Delete(Path.GetTempPath() + "windows10configurerpowershell_reinstall.ps1");
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder PowerShellScript = new StringBuilder();
+            foreach (string item in OptionsList.CheckedItems)
+            {
+                if (item == "Install Windows Photo Viewer")
+                {
+                    Registry.SetValue(@"HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open", "MuiVerb", "@photoviewer.dll,-3043");
+                    Registry.SetValue(@"HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\command", "@", @"hex(2):25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,6f,00,74,00,25,\
+00, 5c, 00, 53, 00, 79, 00, 73, 00, 74, 00, 65, 00, 6d, 00, 33, 00, 32, 00, 5c, 00, 72, 00, 75, 00,
+6e, 00, 64, 00, 6c, 00, 6c, 00, 33, 00, 32, 00, 2e, 00, 65, 00, 78, 00, 65, 00, 20, 00, 22, 00, 25,
+00, 50, 00, 72, 00, 6f, 00, 67, 00, 72, 00, 61, 00, 6d, 00, 46, 00, 69, 00, 6c, 00, 65, 00, 73, 00,
+25, 00, 5c, 00, 57, 00, 69, 00, 6e, 00, 64, 00, 6f, 00, 77, 00, 73, 00, 20, 00, 50, 00, 68, 00, 6f,
+00, 74, 00, 6f, 00, 20, 00, 56, 00, 69, 00, 65, 00, 77, 00, 65, 00, 72, 00, 5c, 00, 50, 00, 68, 00,
+6f, 00, 74, 00, 6f, 00, 56, 00, 69, 00, 65, 00, 77, 00, 65, 00, 72, 00, 2e, 00, 64, 00, 6c, 00, 6c,
+00, 22, 00, 2c, 00, 20, 00, 49, 00, 6d, 00, 61, 00, 67, 00, 65, 00, 56, 00, 69, 00, 65, 00, 77, 00,
+5f, 00, 46, 00, 75, 00, 6c, 00, 6c, 00, 73, 00, 63, 00, 72, 00, 65, 00, 65, 00, 6e, 00, 20, 00, 25,
+00, 31, 00, 00, 00");
+                    Registry.SetValue(@"HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\DropTarget", "Clsid", "{FFE2A43C-56B9-4bf5-9A79-CC6D4285608A}");
+                    Registry.SetValue(@"HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command", "@", @"hex(2):25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,6f,00,74,00,25,\
+00, 5c, 00, 53, 00, 79, 00, 73, 00, 74, 00, 65, 00, 6d, 00, 33, 00, 32, 00, 5c, 00, 72, 00, 75, 00,
+6e, 00, 64, 00, 6c, 00, 6c, 00, 33, 00, 32, 00, 2e, 00, 65, 00, 78, 00, 65, 00, 20, 00, 22, 00, 25,
+00, 50, 00, 72, 00, 6f, 00, 67, 00, 72, 00, 61, 00, 6d, 00, 46, 00, 69, 00, 6c, 00, 65, 00, 73, 00,
+25, 00, 5c, 00, 57, 00, 69, 00, 6e, 00, 64, 00, 6f, 00, 77, 00, 73, 00, 20, 00, 50, 00, 68, 00, 6f,
+00, 74, 00, 6f, 00, 20, 00, 56, 00, 69, 00, 65, 00, 77, 00, 65, 00, 72, 00, 5c, 00, 50, 00, 68, 00,
+6f, 00, 74, 00, 6f, 00, 56, 00, 69, 00, 65, 00, 77, 00, 65, 00, 72, 00, 2e, 00, 64, 00, 6c, 00, 6c,
+00, 22, 00, 2c, 00, 20, 00, 49, 00, 6d, 00, 61, 00, 67, 00, 65, 00, 56, 00, 69, 00, 65, 00, 77, 00,
+5f, 00, 46, 00, 75, 00, 6c, 00, 6c, 00, 73, 00, 63, 00, 72, 00, 65, 00, 65, 00, 6e, 00, 20, 00, 25,
+00, 31, 00, 00, 00");
+                    Registry.SetValue(@"HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\DropTarget", "Clsid", "{60fd46de-f830-4894-a628-6fa81bc0190d}");
+                } else
+                {
+                    string cleaned = item.Substring(8);
+                    foreach (KeyValuePair<string, string> conf in Config)
+                    {
+                        if (conf.Value == cleaned)
+                        {
+                            DisabledApps.AppendLine(conf.Value.Substring(0,conf.Value.Length - 1));
+                            PowerShellScript.AppendLine("Get-AppxPackage *" + conf.Key + "* | Remove-AppxPackage");
+                            break;
+                        }
+                    }
+                }
+            }
+            if (PowerShellScript.Length > 0)
+            {
+                OptionsList.Enabled = false;
+                GoButton.Enabled = false;
+                ReinstallButton.Enabled = false;
+                using (FileStream PSFile = File.Create(Path.GetTempPath() + "windows10configurerpowershell.ps1"))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes(PowerShellScript.ToString());
+                    PSFile.Write(info, 0, info.Length);
+                }
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "powershell.exe";
+                psi.Arguments = "& '" + Path.GetTempPath() + "windows10configurerpowershell.ps1'";
+                Process process = new Process();
+                process.StartInfo.Verb = "runas";
+                process.EnableRaisingEvents = true;
+                process.Exited += new EventHandler(PowerShellExited);
+                process.StartInfo = psi;
+                process.Start();
+            }
+        }
+
+        private void PowerShellExited(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully disabled:\n\n" + DisabledApps.ToString(),"Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            DisabledApps.Clear();
+            OptionsList.Invoke((Action)delegate
+            {
+                OptionsList.Enabled = true;
+            });
+            GoButton.Invoke((Action)delegate
+            {
+                GoButton.Enabled = true;
+            });
+            ReinstallButton.Invoke((Action)delegate
+            {
+                ReinstallButton.Enabled = true;
+            });
+            File.Delete(Path.GetTempPath() + "windows10configurerpowershell.ps1");
+        }
+
+        private void OptionsList_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            CheckedListBox clb = (CheckedListBox)sender;
+            clb.ItemCheck -= OptionsList_ItemCheck;
+            clb.SetItemCheckState(e.Index, e.NewValue);
+            clb.ItemCheck += OptionsList_ItemCheck;
+
+            if (OptionsList.CheckedItems.Count == 0)
+                GoButton.Enabled = false;
+            else
+                GoButton.Enabled = true;
+        }
+
+        private void OptionsList_Click(object sender, EventArgs e)
+        {
+            if (LastCheckedIndex == OptionsList.SelectedIndex) return;
+            LastCheckedIndex = OptionsList.SelectedIndex;
+            OptionsList.SetItemChecked(OptionsList.SelectedIndex, !OptionsList.GetItemChecked(OptionsList.SelectedIndex));
+        }
+    }
+
+    public class Config
+    {
+        public string PowerShellName;
+        public string HumanName;
+    }
+}
